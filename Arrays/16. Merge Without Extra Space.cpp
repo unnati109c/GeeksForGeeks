d9dea3c7b8375d9bec1dcdf0109075d27a47f18f
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-06-23 16:39:18 +0530

    Arrays GFG

---
+++ Arrays/16. Merge Without Extra Space.cpp
@@ -0,0 +1,71 @@
+/* Input: 
+n = 4, arr1[] = [1 3 5 7] 
+m = 5, arr2[] = [0 2 6 8 9]
+Output: 
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9] */
+
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int i = 0, j = 0, k = n - 1;
+            while ( (i <= k) && (j < m) )
+            {
+                if (arr1[i] < arr2[j])
+                    i++;
+                else 
+                {
+                    swap(arr2[j++], arr1[k--]);
+                }
+            }
+           
+            sort(arr1, arr1 + n);
+            sort(arr2, arr2 + m); 
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
