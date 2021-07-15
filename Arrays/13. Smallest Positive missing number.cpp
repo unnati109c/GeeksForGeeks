5fd3c21a27367a8c05a9d3118b71feb0134dabaa
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-07-15 08:37:18 +0530

    Arrays GFG

--- Arrays/13. Smallest Positive missing number.cpp
+++ Arrays/13. Smallest Positive missing number.cpp
@@ -8,45 +8,43 @@ Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
 */
 
-#include<bits/stdc++.h>
-using namespace std;
-
-class Solution{
-    public:
-   
-    int findMissing(int arr[], int n) { 
-        sort(arr, arr+n);
-        // for(int i=0;i<n;i++)
-        // cout<<arr[i]<<" ";
-        // cout<<endl;
-        int k=0;
-        while(arr[k]<0)
-        k++;
-        if(arr[k]!=0 and arr[k]!=1) return 1;  // -7 -5 8 9 then o/p : 1
-        for(int i=k;i<n-1;i++)
+//METHOD 1   
+int findMissing(int arr[], int n) { 
+    sort(arr, arr+n);
+    // for(int i=0;i<n;i++)
+    // cout<<arr[i]<<" ";
+    // cout<<endl;
+    int k=0;
+    while(arr[k]<0)
+    k++;
+    if(arr[k]!=0 and arr[k]!=1) return 1;  // -7 -5 8 9 then o/p : 1
+    for(int i=k;i<n-1;i++)
+    {
+        if(arr[i]==arr[i+1]) continue;  // -9 -2 0 1 1 2 4 then o/p : 3
+        if((arr[i]+1)!=arr[i+1])
         {
-            if(arr[i]==arr[i+1]) continue;  // -9 -2 0 1 1 2 4 then o/p : 3
-            if((arr[i]+1)!=arr[i+1])
-            {
-                return arr[i]+1;
-            }
+            return arr[i]+1;
         }
-        return arr[n-1]+1;
     }
-};
+    return arr[n-1]+1;
+}
 
-int main() { 
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int arr[n];
-        
-        for(int i=0; i<n; i++)cin>>arr[i];
-        
-        Solution ob;
-        cout<<ob.findMissing(arr, n)<<endl;
-    }
-    return 0; 
-}   // } Driver Code Ends
+//METHOD 2
+
+int missingNumber(int arr[], int n) 
+    { 
+       for(int i=0;i<n;i++)
+       {
+           while( (arr[i]>=1 and arr[i]<=n) and (arr[i]!=arr[arr[i]-1]) )
+           {
+               swap(arr[i], arr[arr[i]-1]);
+           }
+       }
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i]!=i+1)
+           return i+1;
+       }
+       return n+1;
+       
+    } 
